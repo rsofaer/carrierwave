c3dfbdda2fa227af91fe383bb126f59b991a318f
Raphael <raphael@joindiaspora.com>
2010-07-16 12:27:59 -0700

    Add mongomapper back in... hopefully with rails 3 support as well.  Untested

--- lib/carrierwave.rb
+++ lib/carrierwave.rb
@@ -94,7 +94,8 @@ elsif defined?(Sinatra)
 end
 
 
-require 'carrierwave/orm/activerecord' if defined?(ActiveRecord)
-require 'carrierwave/orm/datamapper' if defined?(DataMapper)
-require 'carrierwave/orm/sequel' if defined?(Sequel)
-require 'carrierwave/orm/mongoid' if defined?(Mongoid)
+require File.join(File.dirname(__FILE__), "carrierwave", "orm", 'activerecord') if defined?(ActiveRecord)
+require File.join(File.dirname(__FILE__), "carrierwave", "orm", 'datamapper') if defined?(DataMapper)
+require File.join(File.dirname(__FILE__), "carrierwave", "orm", 'sequel') if defined?(Sequel)
+require File.join(File.dirname(__FILE__), "carrierwave", "orm", "mongoid") if defined?(Mongoid)
+require File.join(File.dirname(__FILE__), "carrierwave", "orm", "mongomapper") if defined?(MongoMapper)
